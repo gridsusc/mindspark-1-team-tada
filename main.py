420dd90e88fa2d6d92f79567d5099b7eba12da0e
Aditya Dave <aditya.dave2017@gmail.com>
2022-04-16 11:14:32 -0700

    beautification and using material scoring csv

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
 import re
 import sys
-import csv
 
+import numpy as np
+import pandas as pd
 from PIL import Image
 import streamlit as st
 import pytesseract
 
-matcher = re.compile(r'(\d+)% ([a-zA-Z]+)')
 
-def calculate_sustainability_score(image):
-  txt = pytesseract.image_to_string(Image.open(image))
-  groups = matcher.findall(txt)
-  for group in groups:
-    print(group)
-  return 8
+def calculate_sustainability_score(percentages_materials, material_scoring_key):
+  # Calculate score for the whole item
+  sustainability_score = 0.0
+  for percentage_material in percentages_materials:
+    percentage, material = float(percentage_material[0]) / 100, percentage_material[1].lower()
+    if material in material_scoring_key:
+      sustainability_score += (percentage * material_scoring_key[material])
+  
+  return sustainability_score
 
-def main():
+def main(material_scoring_key):
+  matcher = re.compile(r'(\d+)% ([a-zA-Z]+)')
+
   st.title("Sustainable Shopping Suggester")
 
   # Show upload image option
   uploaded_files = st.file_uploader("Please upload a fabric content tag: ", accept_multiple_files=True)
   
   # Store scores
-  idx, max_score = -1, sys.float_info.min
+  idx, max_score = -1, -sys.float_info.max
 
-  cols = st.columns(len(uploaded_files)) if uploaded_files else list()
+  items = list()
+
+  sustainability_scores = list()
 
   # Calculate score for each file
   for id, uploaded_file in enumerate(uploaded_files):
-      # To read file as bytes:
-      bytes_data = uploaded_file.read()
-      
-      # Display uploaded image
-      with cols[id]:
-        st.write(f"{id + 1}")
-        st.image(bytes_data, use_column_width=True)
+    items.append(uploaded_file.read())
+    
+    txt = pytesseract.image_to_string(Image.open(uploaded_file))
 
-      # Function to parse and get fabric contents
-      sustainability_score = calculate_sustainability_score(uploaded_file)
+    groups = matcher.findall(txt)
 
-      if max_score < sustainability_score:
-        idx, max_score = id, sustainability_score
+    # Function to parse and get fabric contents
+    sustainability_score = calculate_sustainability_score(groups, material_scoring_key)
 
-      st.write(f"#### The sustainability score of cloth {id + 1} is :: {sustainability_score}")
+    sustainability_scores.append(sustainability_score)
+
+    if max_score < sustainability_score:
+      idx, max_score = id, sustainability_score
 
   if idx != -1:
-    st.write(f"### {idx + 1} is the best according to our sustainibility rating of {max_score}.")
+    st.write(f"### Best Item :: {idx + 1:02d} with sustainability score :: {max_score}.")
+
+  if uploaded_files:
+    with st.container():
+
+      cols = st.columns(len(uploaded_files)) if uploaded_files else list()
+      
+      for idx in range(len(sustainability_scores)):
+        col, item, sustainability_score = cols[idx], items[idx], sustainability_scores[idx]
+        
+        # Display uploaded image
+        with col:
+          st.write(f"Item {id + 1:0d} ; Score :: {sustainability_score}")
+          st.image(item, caption=f"Item {id + 1:02d}", width=250)
 
 
 if __name__ == "__main__":
-  main()
+
+  material_scoring = pd.read_csv('./material_scoring.csv', header=0, dtype={"Material": str, "Sustainability_Score": np.float64})
+  material_scoring['Material'] = material_scoring['Material'].str.lower()
+  material_scoring_key = material_scoring.to_dict()
+
+  main(material_scoring_key)
